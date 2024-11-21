50633d063c617a3f83e740d00d609826a7961686
FredieTech <frediezra60@gmail.com>
2024-11-21 08:20:41 +0300

    Create ezra.js

---
+++ fredi/ezra.js
@@ -0,0 +1,16 @@
+var tabCmds = [];
+let cm = [];
+function ezra(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "🤍";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { ezra, Module: ezra, cm };
